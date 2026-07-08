2760ff039b21ce3272d5f0ea6968e6929f3e8dc7
neumanndeveloper <neumanndeveloper@gmail.com>
2026-07-08 09:45:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "relatorio-app-v3";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// INSTALAÇÃO
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+
+  // ativa imediatamente
+  self.skipWaiting();
+});
+
+// ATIVAÇÃO
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
